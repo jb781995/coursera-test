9166bbc28a183840be292d210d37085a3990eef0
Bhardwaj <bhardwajjsh05@gmail.com>
2017-01-01 11:51:40 +0530

    new folder for module1 solution

--- DIApp.js
+++ DIApp.js
@@ -2,14 +2,25 @@
 	'use strict';
 	angular.module('DIApp',[])
 	.controller('DIController',DIController);
+	.controller('MessageController' MessageController);
   
      function DIController($scope,$filter){
-     	$scope.name="Bhardwaj";
+     	$scope.name="jb";
 
      	$scope.upper = function () {
-           var upCase= $filter('upperCase');
+           var upCase= $filter('uppercase');
            $scope.name=upCase($scope.name);
      	};
-     }
+     };
+     
+
+     MessageController.$inject=[$scope];
+     function MessageController($scope){
+        $scope.name2="Bhardwaj";
+     };
+
+     $scope.sayMessage=function(){
+    return "This Method returns a string";
+     };
 
 })();
